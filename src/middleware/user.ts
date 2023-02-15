4c65fa9950c5c6b7bade9a1b7d32c424bb724dbe
원종필 <jp.won@purple.io>
2023-02-16 00:51:58 +0900

    로그아웃 추가 및 middleware chain 수정

--- src/middleware/user.ts
+++ src/middleware/user.ts
@@ -5,18 +5,16 @@ import { User } from "../entities/User/User";
 export default async (req: Request, res: Response, next: NextFunction) => {
     try {
         const token = req.cookies.token;
-        console.log("token",token)
         if (!token) return next();
 
         const { username }: any = jwt.verify(token, process.env.JWT_SECRET!)
 
         const user = await User.findOneBy({ username })
-        console.log("user",user)
         if (!user) throw new Error("Unauthenticated");
 
         // 유저 정보를 res.local.user 에 넣어주기
         res.locals.user = user;
-
+        return next()
     } catch (error) {
         console.log(error);
         return res.status(400).json({error: "user middleware went wrong"})
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -15,6 +15,7 @@ const mapErrors = (errors: Object[]) => {
 }
 
 const me = async (req: Request, res: Response) => {
+    console.log("me=="+res.locals.user);
     return res.json(res.locals.user);
 }
 
@@ -102,8 +103,23 @@ const login = async (req: Request, res: Response) => {
     }
 }
 
+const logout = async (req: Request, res: Response) => {
+    res.set(
+        "Set-Cookie",
+        cookie.serialize("token", "", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            expires: new Date(0),
+            path: "/"
+        })
+    )
+    res.status(200).json({ success: true })
+}
+
 const router = Router();
 router.get("/me", userMiddleware, authMiddleware, me)
 router.post("/register", register);
 router.post("/login", login)
+router.post("/logout", userMiddleware, authMiddleware, logout)
 export default router;
