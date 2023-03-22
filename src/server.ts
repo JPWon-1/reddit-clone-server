98d43000e06f01777062843ebe7c24d30ebbab47
원종필 <jp.won@purple.io>
2023-03-23 01:53:12 +0900

    user router

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import authRoutes from './routes/auth';
 import subRoutes from './routes/subs';
 import postRoutes from './routes/posts';
 import voteRoutes from './routes/votes';
+import userRoutes from './routes/users';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
@@ -27,6 +28,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/subs", subRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/votes", voteRoutes);
+app.use("/api/users", userRoutes);
 
 const port = 4000;
 app.listen(port, async() => {
